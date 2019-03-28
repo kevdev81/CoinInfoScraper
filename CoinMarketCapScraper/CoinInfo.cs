48b77455514cdba7c803f01091574e3d2c64c89d
Kevin Kim <kevinkim.2012@gmail.com>
2019-03-28 10:35:56 -0700

    CoinMarketCap Web Scraper

---
+++ CoinMarketCapScraper/CoinInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoinMarketCapScraper
+{
+    public class CoinInfo
+    {
+        public string Name { get; set; }
+        public string MarketCap { get; set; }
+        public string Price { get; set; }
+        public string Volume { get; set; }
+        public string CirculatingSupply { get; set; }
+        public string Change { get; set; }
+    }
+}
---
+++ CoinMarketCapScraper/Program.cs
@@ -0,0 +1,60 @@
+﻿using AngleSharp.Html.Parser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+
+namespace CoinMarketCapScraper
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var results = new List<CoinInfo>();
+
+            // 1. Download the HTML for the page.
+            // https://coinmarketcap.com/
+
+            var webClient = new WebClient();
+            var html = webClient.DownloadString("https://coinmarketcap.com/");
+
+            // 2. Use CSS selectors to find the table.
+
+            var parser = new HtmlParser();
+            var document = parser.ParseDocument(html);
+            var table = document.QuerySelector(".table");
+            
+            // 3. Loop over every row and create an object for each row.
+            //    (  Name, Market Cap, Volume, Circulating Supply, Change%  )
+
+            var rows = table.QuerySelectorAll("tr").Skip(1);
+
+            foreach (var row in rows)
+            {
+                var coinInfo = new CoinInfo();
+                // coinInfo.Name = row.QuerySelectorAll("td").Skip(1).First().TextContent;
+                coinInfo.Name = row.QuerySelector("td:nth-child(2)").TextContent;
+                coinInfo.MarketCap = row.QuerySelector("td:nth-child(3)").TextContent;
+                coinInfo.Price = row.QuerySelector("td:nth-child(4)").TextContent;
+                coinInfo.Volume = row.QuerySelector("td:nth-child(5)").TextContent;
+                coinInfo.CirculatingSupply = row.QuerySelector("td:nth-child(6)").TextContent;
+                coinInfo.Change = row.QuerySelector("td:nth-child(7)").TextContent;
+
+                results.Add(coinInfo);
+            }
+                   
+            // 4. Print out the results
+
+            foreach (var coinInfo in results)
+            {
+                Console.WriteLine
+                    ($"Name={coinInfo.Name}, " +
+                    $"MarketCap={coinInfo.MarketCap}, " +
+                    $"Price={coinInfo.Price}, " +
+                    $"Volume={coinInfo.Volume}, " +
+                    $"CirculatingSupply={coinInfo.CirculatingSupply}, " +
+                    $"Change={coinInfo.Change}");
+            }
+        }
+    }
+}
